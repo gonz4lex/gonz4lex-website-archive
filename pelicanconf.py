7466ea05f6899ac80336482ae5bd240320e76382
Alex Gonzalez <->
2022-03-05 17:44:41 +0100

    theme changes

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,19 +1,26 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
+from datetime import date
+import time
+import os
+
 
 AUTHOR = 'Alex Gonzalez'
 SITENAME = 'Alex Gonzalez'
 TAGLINE = "Hey! I'm Alex, a developer specialising on machine learning, data science, engineering, visualization and analytics."
-SITEURL = 'https://alexgonzalezc.dev'
-THEME = 'svbhack'
-LOAD_CONTENT_CACHE = True
+SITEURL = 'http://127.0.0.1:8000'
 
 PATH = 'content'
-
+THEME = './themes/svbhack'
+LOAD_CONTENT_CACHE = False
 TIMEZONE = 'Europe/Madrid'
-
 DEFAULT_LANG = 'en'
+DEFAULT_PAGINATION = 10
+DEFAULT_METADATA = {
+    'status': 'draft',
+}
+TYPOGRIFY = True
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
@@ -22,41 +29,33 @@ TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
+GITHUB_URL = 'http://github.com/gonz4lex'
+EMAIL = "hello@alexgonzalezc.dev"
+LINKEDIN_URL = "https://www.linkedin.com/in/alejandro-gonzalez-a05636127/"
+
 # # Blogroll
 # LINKS = (('Github', 'http://github.com/gonz4lex'),
-#          ('Python.org', 'http://python.org/'),
 #          )
 
 # Social widget
 SOCIAL = (
-    ('Github', 'http://github.com/gonz4lex'),
-    # ('Twitter', 'http://twitter.com/gonz4lex_'),
+    ('Github', GITHUB_URL),
+    ('LinkedIn', LINKEDIN_URL),
 )
-
-
-TYPOGRIFY = True
-DEFAULT_PAGINATION = 10
-DEFAULT_METADATA = {
-    'status': 'draft',
-}
 
 # Uncomment following line if you want document-relative URLs when developing
 RELATIVE_URLS = True
 
-# Blog setup
-USER_LOGO_URL = SITEURL + '/images/logo.png'
+USER_LOGO_URL = SITEURL + '/images/avatar.png'
 
 STATIC_PATHS = ['images', 'extra', 'assets']
-HEADER_COVER = "images/last-night.jpg"
-
 
 ARTICLE_PATHS = ['posts',]
 ARTICLE_URL = 'posts/{slug}.html'
 ARTICLE_SAVE_AS = 'posts/{slug}.html'
 
-AUTHOR_PATHS = ['author',]
-AUTHOR_URL = 'author/{slug}.html'
-AUTHOR_SAVE_AS = 'author/{slug}.html'
+AUTHOR_URL = ''
+AUTHOR_SAVE_AS = ''
 
 DIRECT_TEMPLATES = ['index', 'authors', 'categories', 'tags', 'archives']
 
@@ -65,15 +64,20 @@ EXTRA_PATH_METADATA = {
 }
 
 DEFAULT_DATE_FORMAT = '%Y-%m-%d %H:%m'
-NEWEST_FIRST_ARCHIVES = True
-
-GITHUB_URL = 'http://github.com/gonz4lex'
-EMAIL = "hello@alexgonzalezc.dev"
-LINKEDIN_URL = "https://www.linkedin.com/in/alejandro-gonzalez-a05636127/"
+NEWEST_FIRST_ARCHIVES = False
 
 SHOW_SOCIAL_ON_INDEX_PAGE_HEADER = True
 DISPLAY_PAGES_ON_MENU = True
+
 GOOGLE_ANALYTICS = "UA-155317378-1"
 
 import logging
 LOG_FILTER = [(logging.WARN, 'Cannot get modification stamp')]
+
+MARKDOWN = {
+    'extensions' : ['markdown.extensions.codehilite', 'markdown.extensions.extra', 'markdown.extensions.admonition'],
+    'extension_configs': {
+        'markdown.extensions.codehilite': {'css_class': 'highlight','guess_lang': 'False', 'linenums': 'True'},
+    },
+    'output_format': 'html5',
+}
--- publishconf.py
+++ publishconf.py
@@ -11,13 +11,14 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = 'https://alexgonzalezc.dev/'
+SITEURL = 'https://alexgonzalezc.dev'
 RELATIVE_URLS = True
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
 CATEGORY_FEED_ATOM = 'feeds/{slug}.atom.xml'
 
 DELETE_OUTPUT_DIRECTORY = True
+
 
 # Following items are often useful when publishing
 
